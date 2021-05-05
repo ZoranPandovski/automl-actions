c7bba69a350a70bfa0631d0bf0fb46b217f32022
ZoranPandovski <zoran.pandovski@gmail.com>
2021-05-05 22:30:49 +0200

    :flag:

---
+++ mdb.py
@@ -0,0 +1,9 @@
+import mindsdb_native
+
+# Train a predictor with quick_learn
+predictor = mindsdb_native.Predictor(name='test_quick_interface')
+predictor.quick_learn(
+    from_data='https://raw.githubusercontent.com/mindsdb/mindsdb-examples/master/classics/home_rentals/dataset/test.csv',
+    to_predict='rental_price',
+    stop_training_in_x_seconds=20
+)
