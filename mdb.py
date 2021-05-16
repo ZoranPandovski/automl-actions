04f8072394aa363ba79e5d0f724e28dec3a3093f
ZoranPandovski <zoran.pandovski@gmail.com>
2021-05-16 15:32:37 +0200

    Add target param

--- mdb.py
+++ mdb.py
@@ -2,11 +2,12 @@ import mindsdb_native
 import sys
 
 print('HERE **************************')
-print("PARAMETER: ", sys.argv[0])
+print("PARAMETER1: ", sys.argv[0])
+print("PARAMETER2: ", sys.argv[1])
 # Train a predictor with quick_learn
 predictor = mindsdb_native.Predictor(name='test_quick_interface')
 predictor.quick_learn(
-    from_data='https://raw.githubusercontent.com/mindsdb/mindsdb-examples/master/classics/home_rentals/dataset/test.csv',
-    to_predict='rental_price',
+    from_data=sys.argv[0],
+    to_predict=sys.argv[1],
     stop_training_in_x_seconds=20
 )
