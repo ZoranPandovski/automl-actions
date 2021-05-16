b1a2ec0b00df709ee1ebf5200093c926adca28bf
Zoran Pandovski <zoran.pandovski@gmail.com>
2021-05-16 17:03:04 +0200

    Update mdb.py

--- mdb.py
+++ mdb.py
@@ -9,5 +9,5 @@ predictor = mindsdb_native.Predictor(name='test_quick_interface')
 predictor.quick_learn(
     from_data=sys.argv[0],
     to_predict=sys.argv[1],
-    stop_training_in_x_seconds=20
+    stop_training_in_x_seconds=5
 )
