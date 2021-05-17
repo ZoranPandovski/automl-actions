154415626384db1355685ca98a99a5d8cec867bc
Zoran Pandovski <zoran.pandovski@gmail.com>
2021-05-17 13:34:16 +0200

    Update mdb.py

--- mdb.py
+++ mdb.py
@@ -2,12 +2,12 @@ import mindsdb_native
 import sys
 
 print('HERE **************************')
-print("PARAMETER1: ", sys.argv[0])
-print("PARAMETER2: ", sys.argv[1])
+print("PARAMETER1: ", sys.argv[2])
+print("PARAMETER2: ", sys.argv[4])
 # Train a predictor with quick_learn
 predictor = mindsdb_native.Predictor(name='test_quick_interface')
 predictor.quick_learn(
-    from_data=sys.argv[0],
-    to_predict=sys.argv[1],
+    from_data=sys.argv[2],
+    to_predict=sys.argv[4],
     stop_training_in_x_seconds=5
 )
