dcf10a770c8b79e73dd210862fbc676635608206
ZoranPandovski <zoran.pandovski@gmail.com>
2021-05-22 15:49:15 +0200

    Add when input

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import sys
 import os
 import json
 
-# Read env variables send from GH Action
+# Read env variables sent from GH Action
 DATA = os.getenv('INPUT_DATASET')
 TARGET = os.getenv('INPUT_TARGET')
+WHEN_DATA = os.getenv('INPUT_WHEN')
 STOP_TRAINING = os.getenv('INPUT_STOP_TRAINING')
 
 # Train a new model
@@ -16,6 +17,6 @@ predictor.learn(
     stop_training_in_x_seconds=5
 )
 
-result = predictor.predict(when_data={'sqft' : 500})
+result = predictor.predict(when_data=WHEN_DATA)
 output = json.dumps(result[0].explain())
 os.system("echo ::set-output name=prediction::" + output)
